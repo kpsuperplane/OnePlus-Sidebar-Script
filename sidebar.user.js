a6f856a817f9a78f08714460c7fe4550dafdb272
Kevin Pei <kevin-pei@hotmail.com>
2014-10-14 17:09:54 -0400

    Adapted UserScript
    Included Moderator Tools, made code more semantic.

--- sidebar.user.js
+++ sidebar.user.js
@@ -1,105 +1,100 @@
 // ==UserScript==
-// @name         Quick Links for normal users
+// @name         Quick Links for OnePlus Forum Users
 // @namespace    *.oneplus.net*
 // @version      0.1
 // @description  enter something useful
-// @author       Mikasa Ackerman aka Kallen
+// @author       Mikasa Ackerman aka Kallen & Kevin Pei
 // @match        *.oneplus.net*
 // @include        *.oneplus.net*
 // @grant        none
 // ==/UserScript==
 
-document.getElementsByClassName('i-logo-48')[0].style.backgroundImage = "url('http://i.imgur.com/Mh5L3bR.png')";
-document.getElementsByClassName('sidebar')[0].innerHTML = 
-    "<div class='sidebar'>"
-    + "<a href='/forums/off-topic/create-thread' class='redbutton'><span class='inner'><span class='i-new-post'></span> Create Thread</span></a>"
-    + "<div class='section widget-group-no-name widget-container'>"
-    + "<div class='secondaryContent widget OnePlus_WidgetCategories_WidgetRenderer_Languages' id='widget-12'>"
-    + "<h3>"
-    + "<span class='i-world'></span> Quick Links"
-    + "</h3>"
-    + "<ul class='xenforo-list-2cols'>"
-    + "<li><a href='/account/signature/'>Edit Signature</a></li>"
-    + "<li><a href='https://account.oneplus.net/invite/overview'>View Invites</a></li>"
-    + "<li><a href='/conversations/add'>Start PM</a></li>"
-    + "<li><a href='/account/ignored'>Blocked People</a></li>"
-    + "<li><a href='/account/following'>Following</a></li>"
-    + "<li><a href='/watched/threads'>Watched Threads</a></li>"
-    + "<li><a href='/account/likes'>Likes Received</a></li>"
-    + "</ul>"
-    + "<div class='clearfix' style='clear:left'></div>"
-    + "</div>"
-    + "</div>"
-    + "<div class='section widget-group-no-name widget-container'>"
-    + "<div class='secondaryContent widget OnePlus_WidgetCategories_WidgetRenderer_Languages' id='widget-12'>"
-    + "<h3>"
-    + "<span class='i-world'></span> Favorite Threads"
-    + "</h3>"
-    + "<ul class='xenforo-list-2cols'>"
-    + "<li><a href='/threads/the-fellowship.65289'>The Fellowship</a></li>"
-    + "</ul>"
-    + "<div class='clearfix' style='clear:left'></div>"
-    + "</div>"
-    + "</div>"
-    + "<div class='section widget-group-no-name widget-container'>"
-    + "<div class='secondaryContent widget OnePlus_WidgetCategories_WidgetRenderer_Languages' id='widget-12'>"
-    + "<h3>"
-    + "<span class='i-world'></span> Custom Tools"
-    + "</h3>"
-    + "<ul class='xenforo-list-2cols'>"
-    + "<li><a href='/forums/dansk/'>Coming Soon</a></li>"
-    + "</ul>"
-    + "<div class='clearfix' style='clear:left'></div>"
-    + "</div>"
-    + "</div>"
-    + "<div class='section widget-group-no-name widget-container'>"
-    + "<div class='secondaryContent widget OnePlus_WidgetCategories_WidgetRenderer_Categories' id='widget-11'>"
-    + "<h3>"
-    + "<span class='i-world'></span>All Categories"
-    + "</h3>"
-    + "<ul class='xenforo-list-2cols'>"
-    + "<li><a href='/forums/announcements/'>Announcements</a></li>"
-    + "<li><a href='/forums/the-one/'>The One</a></li>"
-    + "<li><a href='/forums/cyanogenmod-11s/'>CyanogenMod 11S</a></li>"
-    + "<li><a href='/forums/dev-roms/'>Development</a></li>"
-    + "<li><a href='/forums/off-topic/'>Off Topic</a></li>"
-    + "<li><a href='/forums/tech-support/'>Tech Support</a></li>"
-    + "<li><a href='/forums/accessories/'>Accessories</a></li>"
-    + "<li><a href='/forums/introduce-yourself/'>Introduce Yourself</a></li>"
-    + "<li><a href='/forums/tech-talk/'>Tech Talk</a></li>"
-    + "<li><a href='/forums/fan-gatherings/'>Fan Gatherings</a></li>"
-    + "<li><a href='/forums/invites/'>Invites</a></li>"
-    + "</ul>"
-    + "<div class='clearfix' style='clear:left'></div>"
-    + "<script>"
-    + "    $('#create-thread-button').on('click', function(e) {"
-    + "        e.preventDefault();"
-    + "        $('#newthread-category').toggle();"
-    + "    });"
-    + "    $('#create-thread-close').on('click', function(e) {"
-    + "        e.preventDefault();"
-    + "        $('#newthread-category').toggle();"
-    + "    });"
-    + "    </script>"
-    + "</div>"
-    + "</div>"
-    + "<div class='section widget-group-no-name widget-container'>"
-    + "<div class='secondaryContent widget OnePlus_WidgetCategories_WidgetRenderer_Stats' id='widget-4'>"
-    + "<h3>"
-    + "<span class='i-stats'></span> Forum Statistics"
-    + "</h3>"
-    + "<div class='pairsJustified'>"
-    + "<dl class='discussionCount'><dt>Discussions:</dt>"
-    + "<dd>114,201</dd></dl>"
-    + "<dl class='messageCount'><dt>Messages:</dt>"
-    + "<dd>5,780,704</dd></dl>"
-    + "<dl class='memberCount'><dt>Members:</dt>"
-    + "<dd>269,363</dd></dl>"
-    + "<dl><dt>Latest Member:</dt>"
-    + "<dd><a href='members/pie004.270323/' class='username' dir='auto'>pie004</a></dd></dl>"
-    + "<dl><dt>Online:</dt>"
-    + "<dd><a href='/online/?type=registered'>2,703</a></dd></dl>"
-    + " "
-    + "</div>"
-    + "</div>"
-    + "</div>"
+//ADD JQUERY SCRIPT ADAPTED FROM https://gist.github.com/eristoddle/4440713
+
+function addJQuery(callback) {
+	var script = document.createElement("script");
+	script.textContent = "(" + callback.toString() + ")();";
+	document.body.appendChild(script);
+}
+function main(){
+	function closeThread(){
+		function getWarningMsg(option, name, title){
+			if(option == 1){
+				return '<p>Hi there @'+name+'!</p><br/><p>It has come to my attention that you have created a thread to ask for invites, however that is not allowed.</p><br/><p>The best way to receive an invite is to hang around the forums, join in on the community discussion and create quality replies and threads.</p><br/><p>If you have any further questions, please read the following thread https://forums.oneplus.net/threads/2-rules-revisited-and-restated.78159/, or feel free to message me via the private message feature.</p><br/><p>Thanks for your understanding!</p><br/><p>Thread closed.</p>';
+			}
+			if(option == 2){
+				var link = prompt("Duplicate link for "+title+"? (enter 0 to skip this)");
+				return '<p>Hi there @'+name+'!</p><br/><p>It appears that this has already been discussed '+(link ? ('at '+link+'.</p>'): 'before.')+'<p>Please search in the future prior to creating a thread, so we don\'t encounter any duplicates :).</p><br/><p>If you have any further questions, feel free to message me via the private message feature.</p><br/><p>Thanks for your understanding!</p><br/><p>Thread closed.</p>';
+			}
+			if(option == 3){
+				return '<p>Hi there @'+name+'!</p><br/><p>It has come to my attention that you have linked to a scalper site, however that is not allowed.</p><br/><p>OnePlus is the one and only official retailer of the OnePlus One at the moment, and buying from other sources puts people at risk of scamming and other dangers.</p><br/><p>If you have any further questions, please read the following thread https://forums.oneplus.net/threads/5-reasons-to-not-buy-the-oneplus-one-from-scalpers.35689/, or feel free to message me via the private message feature.</p><br/><p>Thanks for your understanding!</p>';
+			}
+			if(option == 4){
+				return '<p>Congrats to the winner(s)!</p><br/><p>Thank you @'+name+' for sharing with the OnePlus community :)</p><br/>Invites given, thread closed.</p>';
+			}
+		}
+		function closePost(url, message){
+			$.get(url, function (data) {
+			  var token = data.match(/_csrfToken: \"(.*)\"/)[1];
+			  var dataObject = $(data);
+			  var name = dataObject.find('.message:first').attr('data-author');
+			  var msg = getWarningMsg(message, name, dataObject.find('.titleBar:first h1').text());
+			  $.post(url+'/add-reply', {
+				message_html: msg,
+				_xfRelativeResolver:url,
+				last_date: Date.now(),
+				last_known_date: Date.now(),
+				_xfToken:token,
+				_xfRequestUri:url.replace("https://forums.oneplus.net", ""),
+				_xfNoRedirect:1,
+				_xfResponseType:"json"
+			  }, function(data){
+				if(data.error){
+					alert(data.error[0]);
+					return 0;
+				}
+				 $.post(url+'/quick-update', {
+					'set[discussion_open]':	1,
+					'set[sticky]':1,
+					_xfToken:token,
+					_xfRequestUri:url.replace("https://forums.oneplus.net", ""),
+					_xfNoRedirect:1,
+					_xfResponseType:'json'
+				  }, function(data){});
+					location.reload(1);
+				});
+			  });
+			 
+		}
+		var links = [location.href.replace(/page-\d+/, '').split("#")[0]];
+		window.queue = [];
+		for(var i = 0; i < links.length; i++){
+			var message = 0;
+			while(message < 1 || message > 4){
+				message = Number(prompt("Warning type for \""+(links[i].replace('https://forums.oneplus.net/threads/','').replace('/','').replace('-',' '))+"\": \n1. Begging\n2. Duplicate\n3. Scalpers\n4. Contest over"));
+				if(!message){
+					return; 
+				}
+			}
+			window.queue.push([links[i], message]);
+		}
+		var thisArgs = window.queue.shift();
+		closePost(thisArgs[0], thisArgs[1]);
+	}
+	function sidebar(title){
+		this.wrapper = $('<div class="section widget-group-no-name widget-container"></div>');
+		this.wrapper.append('<div class="secondaryContent widget" id="widget-12"><h3>'+title+'</h3><ul class="custom-inner"></ul><div class="clearfix" style="clear:left"></div></div>');
+		this.content = this.wrapper.find('.custom-inner');
+		this.add = function(elem){
+			this.content.append(elem.wrap('li'));
+		}
+		$('.sidebar .section:first').after(this.wrapper);
+	}
+	var moderatorTools = new sidebar("Moderator Tools");
+	var closeThreadLink = $('<a href="javascript:void(0);">Close Current Thread</a>');
+	closeThreadLink.click(function(){
+		closeThread();
+	});
+	moderatorTools.add(closeThreadLink);
+}
+addJQuery(main);
