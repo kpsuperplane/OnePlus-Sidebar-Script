193d47692066b4af15faccf272310866a0ae5ff2
Sam Prescott <sam@d35.eu>
2014-12-15 19:56:23 +0000

    Updated version number

--- sidebar.user.js
+++ sidebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OnePlus Forum Sidebar
 // @namespace    *.oneplus.net*
-// @version      1.4.1
+// @version      1.4.2
 // @description  enter something useful
 // @author       Mikasa Ackerman aka Kallen, Kevin Pei aka kp1234, Sam Prescott aka sp99, awkward_potato
 // @include      *forums.oneplus.net*
