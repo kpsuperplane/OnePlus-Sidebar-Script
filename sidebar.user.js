32c0711444c88d6f2bd6e16a950fbb44067d7a71
Sam Prescott <sam@d35.eu>
2014-12-22 20:27:57 +0000

    Bump version in order for script to update

--- sidebar.user.js
+++ sidebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OnePlus Forum Sidebar
 // @namespace    *.oneplus.net*
-// @version      2.0.2
+// @version      2.1.2
 // @description  Useful sidebar addon for the OnePlus forum! :)
 // @author       Mikasa Ackerman aka Kallen, Kevin Pei aka kp1234, Sam Prescott aka sp99, awkward_potato
 // @include      *forums.oneplus.net*
